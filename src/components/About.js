e80df6fe2fcb1b2ae733bd1a6c649a51af9b4fa2
Allan Kinyua Murigi <allan.murigi@student.moringaschool.com>
2023-11-11 17:29:07 +0300

    done

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,17 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{props.bio}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+   
+      <Links name={github} linkd={linkedin}/>
     </div>
   );
 }
+import Links from "./Links";
 
 export default About;
---
+++ src/components/Links.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Links(props){
+    return(
+        <div id="links">
+              <p> {props.name}{props.linkd} </p>
+
+        </div>
+    )
+}
+export default Links;
